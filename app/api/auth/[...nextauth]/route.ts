0021ff1cc70d00d4e4114de390834153c298e166
RK576 <kalerushank@gmail.com>
2024-10-07 02:10:49 +0530

    fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -59,6 +59,7 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
+// NextAuth handler for GET and POST requests
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
