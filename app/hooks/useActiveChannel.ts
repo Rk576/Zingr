88aa53ae3bf0d7d43b15415d4610346af6f194fb
RK576 <kalerushank@gmail.com>
2024-10-07 03:17:59 +0530

    tests

--- app/hooks/useActiveChannel.ts
+++ app/hooks/useActiveChannel.ts
@@ -3,11 +3,6 @@ import useActiveList from "./useActiveList";
 import { useEffect, useState } from "react";
 import { pusherClient } from "../libs/pusher";
 
-// Define the structure for the member
-interface Member {
-  id: string;  // or any other relevant member properties
-}
-
 const useActiveChannel = () => {
   const { set, add, remove } = useActiveList();
   const [activeChannel, setActiveChannel] = useState<Channel | null>(null);
@@ -23,16 +18,15 @@ const useActiveChannel = () => {
     channel.bind('pusher:subscription_succeeded', (members: Members) => {
       const initialMembers: string[] = [];
 
-      // Define the member type properly
-      members.each((member: Member) => initialMembers.push(member.id));
+      members.each((member: Record<string, any>) => initialMembers.push(member.id));
       set(initialMembers);
     });
 
-    channel.bind("pusher:member_added", (member: Member) => {
+    channel.bind("pusher:member_added", (member: Record<string, any>) => {
       add(member.id);
     });
 
-    channel.bind("pusher:member_removed", (member: Member) => {
+    channel.bind("pusher:member_removed", (member: Record<string, any>) => {
       remove(member.id);
     });
 
